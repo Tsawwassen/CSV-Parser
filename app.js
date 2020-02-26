c63cf544a9911f0e2b0969f4b6ff3dfa2a50146d
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-02-25 18:19:20 -0800

    Add function to parse CSV file, use the data, and delete the file

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//Using code from W3School (https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp)
-
 const PORT = 8080;
 
 const express = require('express');
@@ -10,21 +8,26 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-//Form upload package
+//Used to receive files from client
 var formidable = require('formidable');
+
+//Use to parse CSV files
+var csv = require('csv-parser');
+
+//used for file system (create, edit, move, delete files )
 var fs = require('fs');
 
+//Basic homepage with form to upload CSV file
 app.get('/', (req, res) => {
-
-  	//Create view for user to upload a CSV file
-  	//Parse the uploaded file on the server so that the information can be used
-
   	res.render('home');
-  	
+});
+
+//Start server
+app.listen(PORT, () => {
+  	console.log('CSV Parser app listening on port ' + PORT);
 });
 
 app.post('/fileupload', (req, res) => {
-	//console.log("inside POST file upload");
 
 	var form = new formidable.IncomingForm();
 
@@ -34,24 +37,53 @@ app.post('/fileupload', (req, res) => {
       	var newpath = './uploads/' + files.filetoupload.name;
       	fs.rename(oldpath, newpath, function (err) {
         	if (err) throw err;
-
-        	//At this point, the file has been uploaded to the server, and saved in the uploads folder of this project
+			
+			//At this point, the file has been uploaded to the server, and saved in the uploads folder of this project
         	//Next step is to open the file on the server, and parse the information.
-        	parseCSV(newpath);
+        	parseCSV(newpath)
 
-        	res.write('File uploaded and moved!');
-        	res.end();
+        	//Let user know the file has been uploaded/parsed
+        	res.write("Content has been parsed");
+    		res.end();
+        	
+        	
         });
     });
 });
 
-app.listen(PORT, () => {
-  	console.log('CSV Parser app listening on port ' + PORT);
 
-});
+
 
 function parseCSV(path){
+	//Code found on https://stackabuse.com/reading-and-writing-csv-files-with-node-js/
 	console.log('parseCSV');
+
+	var fileContent = []
+	var i = 0
+
+	//Note, the below code assumes that headers are in the file.
+	//ie, the first row are the keys for JSON objects
+	fs.createReadStream(path)
+  		.pipe(csv())
+  			.on('data', (row) => {
+  				fileContent.push(row);
+  			})
+  			//The below code is called when the all rows have been read.
+  			//It will call a function to use the data and delete the file
+	  		.on('end', () => {
+	    		useFileData(fileContent);
+	    		deleteFile(path);
+	  		});
+  	
 }
 
+//Use the CSV file data here( ie add to database or do something with the data)
+function useFileData(data){
+	console.log(JSON.stringify(data)); 
+}
+
+//Delete the file after it has been parsed
+function deleteFile(path){
+	fs.unlinkSync(path);
+}
 

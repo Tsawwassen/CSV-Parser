a5280113bfd0246faae9933babdd22518793804d
Tsawwassen <mitchell.rian.smith@gmail.com>
2020-02-25 17:03:28 -0800

    Add basic layout for handlebars view engine

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+//Using code from W3School (https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp)
+
 const PORT = 8080;
 
 const express = require('express');
 const app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
 
-  	res.send('CSV Parser - Hello World!');
+
+  	
+  	//Create view for user to upload a CSV file
+  	//Parse the uploaded file on the server so that the information can be used
+
+  	res.render('home');
+  	
 });
 
 app.listen(PORT, () => {
